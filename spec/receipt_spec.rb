d7de41026b5bf4812a936805b343f441d1f74546
Robert Chang <rchang@fulcrum.net>
2012-06-03 15:05:57 -0700

    Updated receipt tests

--- spec/receipt_spec.rb
+++ spec/receipt_spec.rb
@@ -55,7 +55,7 @@ describe "Receipt" do
     input = Array.new ["1 imported bottle of perfume at 27.99", "1 bottle of perfume at 18.99", "1 packet of headache pills at 9.75", "1 box of imported chocolates at 11.25"]
     receipt = Receipt.new input
     receipt.calculate
-    receipt.receipt_total.round(2).should eql 74.68
+    receipt.receipt_total.should eql 74.68
     receipt.to_s.should eql "1 imported bottle of perfume: 32.19\r\n1 bottle of perfume: 20.89\r\n1 packet of headache pills: 9.75\r\n1 box of imported chocolates: 11.85\r\nSales taxes: 6.70\r\nTotal: 74.68"
   end
 
@@ -63,7 +63,7 @@ describe "Receipt" do
     input = Array.new ["1 can of soup at 0.85", "1 bowl of cereal at 10.45", "10 cases of nuts at 1.50", "20 imported bottles of shampoo at 2.50"]
     receipt = Receipt.new input
     receipt.calculate
-    receipt.receipt_total.round(2).should eql 86.45
+    receipt.receipt_total.should eql 86.45
     receipt.to_s.should eql "1 can of soup: 0.95\r\n1 bowl of cereal: 11.50\r\n10 cases of nuts: 16.50\r\n20 imported bottles of shampoo: 57.50\r\nSales taxes: 10.15\r\nTotal: 86.45"
   end
 
