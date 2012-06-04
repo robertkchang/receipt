e7c018a72c6e0ed4e8df9ed5be027355903a6584
Robert Chang <rchang@fulcrum.net>
2012-06-04 11:40:42 -0700

    Refactored Exempted class

--- lib/exempted.rb
+++ lib/exempted.rb
@@ -5,17 +5,12 @@ class Exempted
 
   EXEMPTED = %w(BOOK CHOCOLATE PILL)
 
-  @@pluralized_exempted_list = Array.new
+  @@pluralized_exempted_list = EXEMPTED.inject(Array.new) { |list, itemName|
+    list << itemName
+    list << itemName + 'S'
+  }
 
-  # Lazy init the pluralized exempted list
   def self.list
-    if @@pluralized_exempted_list.empty?
-      @@pluralized_exempted_list = EXEMPTED.inject(Array.new) { |list, itemName|
-        list << itemName
-        list << itemName + 'S'
-      }
-    end
-
-    @@pluralized_exempted_list
+    return @@pluralized_exempted_list
   end
 end
