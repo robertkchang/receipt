b94d775ba29112e718e11a5d0a0e34b93572c01b
Robert Chang <rchang@fulcrum.net>
2012-06-06 11:10:00 -0700

    Added comment to ShoppingCart

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -2,13 +2,15 @@ require_relative 'item'
 #
 # This class represents a shopping cart of items
 #
+# Accepts a string in the format: {qty} {description} at {price}
+#
 class ShoppingCart
 
   attr_reader :list
 
   # Constructor
-  def initialize input
-    @list = input.inject(Array.new) { |list, itemStr| list << Item.new(itemStr)}
+  def initialize inputStr
+    @list = inputStr.inject(Array.new) { |list, itemStr| list << Item.new(itemStr)}
   end
 
 end
